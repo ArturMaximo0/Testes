6bab37c66ded52cae01d84124ac13439c036d684
BOTteste <103906459+BOTteste@users.noreply.github.com>
2024-12-08 13:45:48 -0300

    update koximo

--- koximo.py
+++ koximo.py
@@ -1,18 +1,18 @@
-nome = input("digite seu nome:")
+nome = input("Digite seu nome:")
 print ("*************************************************************************************************************************************")
-print ("*koximo, um robô que te ajuda nas escolhas e alternativas que você ira enfrentar, ele pode ser muito util em momentos decisivos     *")
-print ("*koximo - Ola meu nome e koximo eu sou seu robo assistente nessa jornada de escolhas, segundo meu sistema seu nome e:"+ nome,"          *")
-print ("*esse jogo é um mundo de escolhas incriveis, você é um aluno(a)japones,tentando fazer as melhores escolhas mas o que você irá fazer?*")
+print ("*Koximo, um robô que te ajuda nas escolhas e alternativas que você ira enfrentar, ele pode ser muito util em momentos decisivos     *")
+print ("*Koximo - Oi! Meu nome é Koximo eu sou seu robo assistente nessa jornada de escolhas, segundo meu sistema seu nome É:"+ nome,"          *")
+print ("*Esse jogo é um mundo de escolhas incriveis, você é uma aluna japonesa,tentando fazer as melhores escolhas mas o que você irá fazer?*")
 print ("*************************************************************************************************************************************")
-print ("você está em sua casa, você acabou de acordar em um belo dia ensolarado")
-print ("seu despertador toca, voce acorda e coloca seu uniforme, desce para a cozinha, toma seu cafe da manha e vai para a escola")
-print ("no caminho voce encontra sua melhor amiga mahari")
-print ("oque voce faz? (opcão 1: ir falar com ela e ir pra escola com ela, opcão 2 fingir fingir não viu ela  e seguir se caminho.)")
+print ("Você está em sua casa, você acabou de acordar em um belo dia ensolarado")
+print ("Seu despertador toca, você acorda e coloca seu uniforme, desce para a cozinha, toma seu cafe da manha e vai para a escola")
+print ("No caminho você encontra sua melhor amiga Mahari")
+print ("Oque você faz? (Opcão 1: Ir falar com ela e ir pra escola com ela, opcão 2: Fingir não viu ela  e seguir se caminho.)")
 escolha1 =input("para escolher a opcão 1 digite: 1 para escolher a opcão 2 digite:2 :")
 #falar com a mahari
 if escolha1 == '1':
   print("Você encontra Mahari,sua melhor amiga desde a infancia.")
-  print("mahari - Oi! quanto tempo, não nos vemos!")
+  print("mahari - Oi! quanto tempo "+ nome)
 #não falar com a mahari
 if escolha1 == '2':
   print ("Você vai para a escola sozinha.")
