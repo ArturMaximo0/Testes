4343d1b2aaf98e974ca1d1dbe884c7df72987f7e
BOTteste <103906459+BOTteste@users.noreply.github.com>
2024-12-08 13:33:15 -0300

    add 2 choice in koximo

--- koximo.py
+++ koximo.py
@@ -7,9 +7,12 @@ print ("************************************************************************
 print ("você está em sua casa, você acabou de acordar em um belo dia ensolarado")
 print ("seu despertador toca, voce acorda e coloca seu uniforme, desce para a cozinha, toma seu cafe da manha e vai para a escola")
 print ("no caminho voce encontra sua melhor amiga mahari")
-print ("oque voce faz? (opcão 1: ir falar com ela e ir pra escola com ela, opcão 2 fingir fingir nao viu ela  e seguir se caminho)")
+print ("oque voce faz? (opcão 1: ir falar com ela e ir pra escola com ela, opcão 2 fingir fingir não viu ela  e seguir se caminho.)")
 escolha1 =input("para escolher a opcão 1 digite: 1 para escolher a opcão 2 digite:2 :")
-
+#falar com a mahari
 if escolha1 == '1':
-  print("Você encontra Mahari,sua melhor amiga desde a infancia")
-  print("mahari - Oi! quanto tempo, não nos vemos")
+  print("Você encontra Mahari,sua melhor amiga desde a infancia.")
+  print("mahari - Oi! quanto tempo, não nos vemos!")
+#não falar com a mahari
+if escolha1 == '2':
+  print ("Você vai para a escola sozinha.")
